f3b38f4c5c97587f2af8ca1c3488ca61ed6d85c8
Vishal <vishal@gmail.com>
2024-12-20 18:03:54 +0530

    second commit

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ pdf = FPDF(orientation="P", unit="mm",format="A4")
 
 pdf.add_page()
 pdf.set_font(family="Times",style="B",size=12)
-pdf.cell(w=0,h=12,txt="Vishal here",align="L",ln=1,border=1)
+pdf.cell(w=0,h=12,txt="Vishal here",align="L",ln=1)
 
+pdf.add_page()
+pdf.set_font(family="Arial",style="B",size=12)
+pdf.cell(w=0,h=12,txt="Vishal There",align="L",ln=1)
 pdf.output("output.pdf")
