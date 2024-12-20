9ad93724ca589dff0ce63d6a19877f1ab0b1af17
Vishal <vishal@gmail.com>
2024-12-20 18:00:20 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from fpdf import FPDF
+
+pdf = FPDF(orientation="P", unit="mm",format="A4")
+
+pdf.add_page()
+pdf.set_font(family="Times",style="B",size=12)
+pdf.cell(w=0,h=12,txt="Vishal here",align="L",ln=1,border=1)
+
+pdf.output("output.pdf")
